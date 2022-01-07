4928c0664d6368810fc12a7d56440e68b760986f
kelj171998 <44838262+kelj171998@users.noreply.github.com>
2022-01-07 22:07:14 +0100

    Word extractor list is now sorted
    - Renamed to word_extractor.py
    - Update with sorted list
    - New text file list uploaded

---
+++ word_extractor.py
@@ -0,0 +1,41 @@
+import sys
+from itertools import permutations
+
+def fullPermList(word):
+    word_len = len(word)
+    long_list = []
+    for cur_word_len in range(word_len, 0, -1):
+        listWords = findWordPermutations(word, cur_word_len)
+        long_list += listWords.copy()
+    return long_list
+
+def findWordPermutations(word, wordLen):
+    return [''.join(p) for p in permutations(word, wordLen)]
+
+def findWordList(word, file):
+    perms = fullPermList(word)
+    newList = [e.strip() for e in file.readlines()]
+    matchList = []
+    for perm in perms:
+        for word in newList:
+            if perm == word:
+                matchList.append(word)
+            #print(f'{perm} == {word} -> {foundWord}')
+
+    return set(matchList)
+
+def main():
+    file = open('sven.txt', 'r', encoding='UTF-8')
+    word = sys.argv[1]
+    wordSet = findWordList(word, file)
+    sortedList = sorted(list(wordSet), key=len, reverse=True)
+
+    if sortedList == set():
+        print('No result found')
+
+    for word in sortedList:
+        print(word)
+
+    file.close()
+
+main() # Remove this temporarily when running tests
