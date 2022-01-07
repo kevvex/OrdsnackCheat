46fbbd09c66476b849ee4ccf9ff4f9bf5774860a
kelj171998 <44838262+kelj171998@users.noreply.github.com>
2022-01-07 20:51:35 +0100

    Permutations of word subsets
    Now possible to reveal fewer letter combinations
    of a word even if the word is longer.

--- revealWords.py
+++ revealWords.py
@@ -1,34 +1,40 @@
 import sys
 from itertools import permutations
 
-def findWordPermutations(word):
-    return [''.join(p) for p in permutations(word)]
+def fullPermList(word):
+    word_len = len(word)
+    long_list = []
+    for cur_word_len in range(word_len, 0, -1):
+        listWords = findWordPermutations(word, cur_word_len)
+        long_list += listWords.copy()
+    return long_list
 
-def findWordList(lettersInWord, file):
-    perms = findWordPermutations(lettersInWord)
+def findWordPermutations(word, wordLen):
+    return [''.join(p) for p in permutations(word, wordLen)]
+
+def findWordList(word, file):
+    perms = fullPermList(word)
     newList = [e.strip() for e in file.readlines()]
     matchList = []
     for perm in perms:
         for word in newList:
             if perm == word:
                 matchList.append(word)
-            #print(f'{perm} == {word}')
+            #print(f'{perm} == {word} -> {foundWord}')
 
     return set(matchList)
 
-def printWords(wordList):
-    for word in wordList:
-        print(word)
+def main():
+    file = open('svenska-ord.txt', 'r', encoding='UTF-8')
+    word = sys.argv[1]
+    wordList = findWordList(word, file)
 
     if wordList == set():
         print('No result found')
 
-def main():
-    file = open('sven.txt', 'r', encoding='UTF-8')
-    word = sys.argv[1]
-    wordList = findWordList(word, file)
-    printWords(wordList)
-    
+    for word in wordList:
+        print(word)
+
     file.close()
 
-main()
+main() # Remove this temporarily when running tests
